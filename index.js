9b67ff2a28b4e9f89dde76a87cca58a71203f06a
sam-malanchuk <malanchuksam@gmail.com>
2019-09-04 19:50:11 -0400

    moved endpoint from index to server file. Added password encryption using bcryptjs

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 const server = require('./server.js');
 
-const Users = require('./users-model.js');
-
-server.post('/api/users', (req, res) => {
-  const userData = req.body;
-
-  Users.registerUser(userData)
-    .then(result => {
-      console.log(result);
-      res.status(201).json(result);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({err});
-    })
-});
-
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const helmet = require('helmet');
+const bcrypt = require('bcryptjs');
+
+const Users = require('./users-model.js');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.status(200).json("You hit the root endpoint!");
+server.post('/api/users', (req, res) => {
+  const userData = req.body;
+
+  userData.password = bcrypt.hashSync(userData.password, 6);
+
+  Users.registerUser(userData)
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(err => {
+      res.status(500).json({err});
+    })
 });
 
 module.exports = server;
