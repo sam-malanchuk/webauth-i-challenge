0cd68edd6dc70b33508676a71079a5c01f516050
sam-malanchuk <malanchuksam@gmail.com>
2019-09-03 21:58:03 -0400

    created the database and start server

---
+++ users-model.js
@@ -0,0 +1,19 @@
+const db = require('./data/db-config.js');
+
+module.exports = {
+  regUser,
+  loginUser,
+  getUsers
+}
+
+function regUser() {
+
+}
+
+function loginUser() {
+
+}
+
+function getUsers() {
+
+}
